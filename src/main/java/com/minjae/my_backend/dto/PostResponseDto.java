5c66363b82596b81c26b8ae0fdd9a585aa6f993b
Minjae0-2 <lawpea38@gmail.com>
2025-08-18 19:14:57 +0900

    feat: Add Post entity and repository
    - Add Post entity to map the posts table in the database.
    - Define fields such as title, content, author, and timestamps.
    - Use @Builder pattern for safe object creation.
    -Add PostRepository interface using Spring Data JPA for CRUD operations.

--- src/main/java/com/minjae/my_backend/dto/PostResponseDto.java
+++ src/main/java/com/minjae/my_backend/dto/PostResponseDto.java
@@ -1,18 +1,19 @@
 package com.minjae.my_backend.dto;
 
+import com.minjae.my_backend.domain.Post;
 import lombok.Getter;
 import java.time.LocalDateTime;
 
 @Getter
 public class PostResponseDto {
-    private final long id;
+    private final Long id;
     private final String title;
     private final String content;
     private final String author;
     private final LocalDateTime createdAt;
     private final LocalDateTime modifiedAt;
 
-    //repository 객체 변환 필요
+    //entity 객체 변환
     private PostResponseDto(Post entity){
         this.id = entity.getId();
         this.title = entity.getTitle();
@@ -22,7 +23,7 @@ public class PostResponseDto {
         this.modifiedAt = entity.getModifiedAt();
     }
 
-    //repository 객체 변환 - from 메소드로
+    //from 메소드로만 객체 생성
     public static PostResponseDto from (Post entity){
         return new PostResponseDto(entity);
     }
