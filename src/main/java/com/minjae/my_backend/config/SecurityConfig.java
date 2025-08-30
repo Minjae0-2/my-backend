d754d6fd3f12f419dd2abab2ea363be2aa924b91
Minjae0-2 <lawpea38@gmail.com>
2025-08-30 16:47:52 +0900

    feat: Implement JWT Authentication Filter
    - Add token validation and parsing logic to JwtUtil.
    - Create JwtAuthenticationFilter to verify tokens on every request.
    - Register the filter in SecurityConfig and set session policy to STATELESS.

--- src/main/java/com/minjae/my_backend/config/SecurityConfig.java
+++ src/main/java/com/minjae/my_backend/config/SecurityConfig.java
@@ -1,14 +1,25 @@
 package com.minjae.my_backend.config;
 
+import com.minjae.my_backend.domain.UserRepository;
+import com.minjae.my_backend.jwt.JwtAuthenticationFilter;
+import com.minjae.my_backend.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -17,10 +28,15 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                //세션을 HttpSession -> STATELESS로 하면서 상태 비저장 = JWT
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/users/signup","/api/users/login").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                //필터 추가하기
+                .addFilterBefore(new JwtAuthenticationFilter(jwtUtil,userRepository), UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
--- src/main/java/com/minjae/my_backend/jwt/JwtUtil.java
+++ src/main/java/com/minjae/my_backend/jwt/JwtUtil.java
@@ -1,14 +1,16 @@
 package com.minjae.my_backend.jwt;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 
+@Slf4j(topic =  "JwtUtil") //로그 추가
 @Component
 public class JwtUtil {
 
@@ -20,6 +22,7 @@ public class JwtUtil {
         this.accessTokenValidity = accessTokenValidity;
     }
 
+    //토큰 생성
     public String createToken(String email){
         Date now = new Date();
         Date validity = new Date(now.getTime()+this.accessTokenValidity); //만료 시간
@@ -30,5 +33,32 @@ public class JwtUtil {
                 .setExpiration(validity) // 토큰 만료된 시간
                 .signWith(key, SignatureAlgorithm.HS256) //사용할 암호화 알고리즘과 비밀키
                 .compact(); //JWT 문자열 만들어 반환
+    }
+
+    // 토큰에서 이메일 추출
+    public String getEmailFromToken(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key) // 토큰 검증하기 위해 key 설정
+                .build() // 파서 객체 생성
+                .parseClaimsJws(token)// 서명 검증, 토큰 파싱하고 Claims 객체 얻음 (토큰에 담긴 데이터 덩어리)
+                .getBody() // Claims 객체 가져옴
+                .getSubject(); //subject 가져옴 - 이메일
+    }
+
+    //토큰 유효성 검증
+    public boolean validateToken(String token){
+        try{
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        }catch(SignatureException | MalformedJwtException e){ //토큰의 서명이 유효하지 않을 때, 토큰의 형식이 잘못되었을 때
+            log.error("Invalid JWT signature, 유효하지 않은 JWT 서명입니다.");
+        }catch(ExpiredJwtException e){
+            log.error("Expired JWT token, 만료된 JWT 토큰 입니다.");
+        }catch(UnsupportedJwtException e){ //ex.JWE
+            log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
+        }catch(IllegalArgumentException e){ //토큰 문자열이 비어있거나 null같이 잘못된 인자 전달
+            log.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
+        }
+        return false;
     }
 }
